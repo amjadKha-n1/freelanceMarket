d63f6000809e3757d460f08390907fe336924e3f
Amjad Khan <amjad22558@gmail.com>
2026-04-16 23:24:06 +0300

    fixed some bugs

--- server/app.js
+++ server/app.js
@@ -24,13 +24,6 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'https://freelance-marketplace-lvyx.onrender.com');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.sendStatus(200);
-});
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
