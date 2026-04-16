ca7f725027ad4ecafb0da82855c4c79a2e882a65
Amjad Khan <amjad22558@gmail.com>
2026-04-16 23:01:10 +0300

    fixed some bugs

--- server/app.js
+++ server/app.js
@@ -23,16 +23,9 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: true,
   credentials: true,
 }));
-
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
