271f7faaea232bd54f875ecfea82d86446a42bf7
Amjad Khan <amjad22558@gmail.com>
2026-04-17 23:06:15 +0300

    fixing

--- server/app.js
+++ server/app.js
@@ -18,22 +18,10 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: function (origin, callback) {
-    const allowedOrigins = [
-      "http://localhost:5173",
-      "https://freelance-marketplace-lvyx.onrender.com"
-    ];
-
-    // allow requests with no origin (like Postman)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
+  origin: [
+    "http://localhost:5173",
+    "https://freelance-marketplace-lvyx.onrender.com"
+  ]
 }));
 
 
