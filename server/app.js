e712e723a2a8e36866cc4fd4a5ff1d70065fd2c5
Amjad Khan <amjad22558@gmail.com>
2026-04-17 23:17:42 +0300

    fixing

--- server/app.js
+++ server/app.js
@@ -17,14 +17,22 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://freelance-marketplace-lvyx.onrender.com"
-  ],
-  credentials: true
-}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://freelance-marketplace-lvyx.onrender.com");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true });
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
