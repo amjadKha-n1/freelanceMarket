7abbd0caaa180ea4fe9d7082be1481b04b700bba
Amjad Khan <amjad22558@gmail.com>
2026-04-17 23:21:31 +0300

    fixing

--- server/app.js
+++ server/app.js
@@ -31,9 +31,6 @@ app.use((req, res, next) => {
 
 app.use(cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true });
-
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.set("layout", "layout");
